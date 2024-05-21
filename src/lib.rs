f3500175386e2a278cb5491202d3f1e8845ba14b
alexhroom <alex.room@btinternet.com>
2024-05-21 19:24:30 +0100

    made rust code shorter via macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,22 @@ use pyo3::exceptions::PyValueError;
 use num_complex::Complex;
 extern crate filon as filon_rs;
 
-#[pyfunction]
-fn filon_tab_sin(ftab: Vec<Complex<f64>>, a: f64, b: f64, sin_coeff: f64) -> PyResult<Complex<f64>> {
-    match filon_rs::filon_tab_sin(ftab, a, b, sin_coeff) {
-        Ok(result) => Ok(result),
-        Err(error) => Err(PyValueError::new_err(error.to_string()))
+/// macro that wraps filon function as pyfunction
+macro_rules! pyo3ise {
+    ($fn_name:ident) => {
+    #[pyfunction]
+    fn $fn_name(ftab: Vec<Complex<f64>>, a: f64, b: f64, coeff: f64) -> PyResult<Complex<f64>> {
+        match filon_rs::$fn_name(ftab, a, b, coeff) {
+            Ok(result) => Ok(result),
+            Err(error) => Err(PyValueError::new_err(error.to_string()))
+            }
+        }
     }
 }
 
-#[pyfunction]
-fn filon_tab_cos(ftab: Vec<Complex<f64>>, a: f64, b: f64, cos_coeff: f64) -> PyResult<Complex<f64>> {
-    match filon_rs::filon_tab_cos(ftab, a, b, cos_coeff) {
-        Ok(result) => Ok(result),
-        Err(error) => Err(PyValueError::new_err(error.to_string()))
-    }
-}
+pyo3ise!(filon_tab_sin);
+pyo3ise!(filon_tab_cos);
 
-
-/// A Python module implemented in Rust.
 #[pymodule]
 fn pyfilon(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(filon_tab_sin, m)?)?;
